1ce5657941e9bd095a783870797ba3b7c7bd7a09
stas <02stas02@gmail.com>
2019-11-09 14:30:32 +0300

    start game

---
+++ ballGame.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from random import randrange as rnd, choice
+import time
+root = Tk()
+root.geometry('800x600')
+
+canv = Canvas(root,bg='white')
+canv.pack(fill=BOTH,expand=1)
+
+colors = ['red','orange','yellow','green','blue']
+def new_ball():
+    canv.delete(ALL)
+    x = rnd(100,700)
+    y = rnd(100,500)
+    r = rnd(30,50)
+    canv.create_oval(x-r,y-r,x+r,y+r,fill = choice(colors), width=0)
+    root.after(1000,new_ball)
+
+
+def click(event):
+    print('click')
+
+new_ball()
+canv.bind('<Button-1>', click)
+mainloop()
